e2765540950f9e715d39217668c55f19eea09f75
Zinah <zinah.poulus@gmail.com>
2026-03-28 10:49:44 +0100

    build the meanu file

--- js/_menu.js
+++ js/_menu.js
@@ -1,3 +1,11 @@
-createMenu('Zinah sajt', [
-  { name: 'Start', script: 'start.js' }
+createMenu('Student Mental Health Analysis', [
+  { name: 'Overview', script: 'overview.js' },
+
+  { name: 'Analysis', sub: [
+    { name: 'Sleep Analysis', script: 'sleep-analysis.js' },
+    { name: 'Academic Pressure', script: 'pressure-analysis.js' },
+    { name: 'Work Hours', script: 'work-analysis.js' }
+  ]},
+
+  { name: 'Conclusion', script: 'conclusion.js' }
 ]);
--- js/start.js
+++
@@ -1,5 +0,0 @@
-addMdToPage(`
-  # welcome!
-  Nu ska jag lära mig visualisera statistik
-`);
-
